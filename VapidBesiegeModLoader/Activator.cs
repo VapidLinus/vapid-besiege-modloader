094d0ffd4420772d2053bd7504a92075cbb6dcd7
Linus Närkling-Larsson <VapidLinus@gmail.com>
2015-06-05 01:42:52 +0200

    Initial commit

---
+++ VapidBesiegeModLoader/Activator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Vapid.ModLoader
+{
+	public static class Activator
+	{
+		private static bool activated;
+
+		public static void Activate()
+		{
+			if (!activated)
+			{
+				new GameObject("Vapid's ModLoader").AddComponent<VapidModLoader>();
+				activated = true;
+			}
+		}
+	}
+}
---
+++ VapidBesiegeModLoader/VapidModLoader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Vapid.ModLoader
+{
+    public class VapidModLoader : MonoBehaviour
+    {
+		string log = "";
+
+		void Awake()
+		{
+			Application.RegisterLogCallback(OnLog);
+
+			Debug.Log("VapidModLoader activated.");
+
+			DontDestroyOnLoad(this);
+		}
+
+		void OnLog(string log, string trace, LogType type)
+		{
+			this.log += string.Format("[{0}] {1}\n", type.ToString(), log);
+		}
+
+		void OnGUI()
+		{
+			var rect = new Rect(20, 40, 200, 2000);
+			GUI.Label(rect, log);
+		}
+    }
+}
